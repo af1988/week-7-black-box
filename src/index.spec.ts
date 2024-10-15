134636b797681ad99751dac6c51d15994f8dd7e7
josie <josieek4@gmail.com>
2024-10-15 10:50:57 -0400

    test numbers

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -18,4 +18,53 @@ describe('Calculator', (): void => {
 
   });
 
+  it('should display `2` when pressOne() is invoked', (): void => {
+
+    calculator.pressTwo();
+    calculator.pressEquals();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('2');
+
+  });
+
+  it('should display `3` when pressOne() is invoked', (): void => {
+
+    calculator.pressThree();
+    calculator.pressEquals();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('3');
+
+  });
+
+  it('should display `4` when pressOne() is invoked', (): void => {
+
+    calculator.pressFour();
+    calculator.pressEquals();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('4');
+
+  });
+
+  it('should display `5` when pressOne() is invoked', (): void => {
+
+    calculator.pressFive();
+    calculator.pressEquals();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('5');
+  });
+
+  it('should display `6` when pressOne() is invoked', (): void => {
+
+    calculator.pressSix();
+    calculator.pressEquals();
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('6');
+
+  });
+
 });
